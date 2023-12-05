b6fa2caf59c0e70605d2f1c7fd7ba7d76e3da147
Angel <angelgutierrezm17@gmail.com>
2023-12-05 00:30:27 -0300

    Agregado de la función de eliminar deseos de la lista

---
+++ js/script.js
@@ -0,0 +1,32 @@
+window.addEventListener("DOMContentLoaded", function() {
+    const inputDeseo = document.querySelector(".deseoInput");
+    const submitBtn = document.querySelector(".submit-btn");
+    const listaDeseos = document.getElementById("listaDeseos");
+
+    submitBtn.addEventListener("click", function(){
+        const nuevoDeseo = inputDeseo.value.trim();
+        if (nuevoDeseo !== "") {
+            const deseoId = Date.now();
+
+            const nuevoElemento = document.createElement("li");
+            nuevoElemento.classList.add("deseo");
+            nuevoElemento.setAttribute("data-deseo-id", deseoId);
+            nuevoElemento.innerHTML= `
+                ${nuevoDeseo}
+                <button class="eliminar-btn" data-deseo-id="${deseoId}"> X </button>
+            `;
+
+            listaDeseos.appendChild(nuevoElemento);
+            inputDeseo.value= "";
+
+            const eliminarBtn = nuevoElemento.querySelector(".eliminar-btn");
+            eliminarBtn.addEventListener("click", function (){
+                const deseoId = eliminarBtn.getAttribute("data-deseo-id");
+                const elementoAEliminar = document.querySelector(`[data-deseo-id]`);
+                if (elementoAEliminar) {
+                    elementoAEliminar.remove();
+                }
+            })
+        }
+    })
+})
